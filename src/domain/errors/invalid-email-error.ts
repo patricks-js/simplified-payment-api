4b5f630e98d310f1aa6aba78c9b6edf9124075cb
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2023-11-30 12:47:46 -0300

    refactor: invalid email error

--- src/domain/errors/invalid-email-error.ts
+++ src/domain/errors/invalid-email-error.ts
@@ -1,6 +1,4 @@
-import { type DomainError } from './domain-error'
-
-export class InvalidEmailError extends Error implements DomainError {
+export class InvalidEmailError extends Error {
   constructor (email: string) {
     super(`The email ${email} is invalid.`)
     this.message = 'InvalidEmailError'
