2c54653025f6701953d781fdeea998a01e50689f
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-02-21 00:26:28 -0300

    remove all eslint related files and setup biome as linter

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { NestFactory } from "@nestjs/core";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
