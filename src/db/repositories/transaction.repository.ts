f59d9f245dc78333d0628ad54459aec3117e58a3
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-03-19 23:11:18 -0300

    add `debit` and `credit` operations in transaction repository

--- src/db/repositories/transaction.repository.ts
+++ src/db/repositories/transaction.repository.ts
@@ -1,7 +1,88 @@
+import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma.service";
+import { Wallet } from "@prisma/client";
 
+type TransactionOperationInput = {
+  amount: number;
+  walletId: number;
+  userId: number;
+};
+
+@Injectable()
 export class TransactionRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  async debit({ amount, walletId, userId }: TransactionOperationInput) {
+    const wallet = await this.prisma.wallet.findUnique({
+      where: {
+        id: walletId,
+        userId,
+      },
+    });
+
+    if (!wallet) {
+      throw new Error("Wallet not found");
+    }
+
+    return this.prisma.wallet.update({
+      where: {
+        id: walletId,
+      },
+      data: {
+        balance: {
+          decrement: amount,
+        },
+      },
+    });
+  }
+
+  async credit({ amount, walletId, userId }: TransactionOperationInput) {
+    const wallet = await this.prisma.wallet.findUnique({
+      where: {
+        id: walletId,
+        userId,
+      },
+    });
+
+    if (!wallet) {
+      throw new Error("Wallet not found");
+    }
+
+    return this.prisma.wallet.update({
+      where: {
+        id: walletId,
+      },
+      data: {
+        balance: {
+          increment: amount,
+        },
+      },
+    });
+  }
+
+  async rollback(debitWallet: Wallet, creditWallet: Wallet, amount: number) {
+    await this.prisma.wallet.update({
+      where: {
+        id: debitWallet.id,
+      },
+      data: {
+        balance: {
+          increment: amount,
+        },
+      },
+    });
+
+    await this.prisma.wallet.update({
+      where: {
+        id: creditWallet.id,
+      },
+      data: {
+        balance: {
+          decrement: amount,
+        },
+      },
+    });
+  }
 
   async register({
     amount,
